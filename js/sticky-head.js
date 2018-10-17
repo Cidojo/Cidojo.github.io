ad8db7018fd083c5b3f84c560098abd16072167d
Cidojo <cidell@mail.ru>
2018-10-17 17:11:23 +0300

    filling content

--- js/sticky-head.js
+++ js/sticky-head.js
@@ -1,16 +1,15 @@
 
-var header = document.querySelector(".main-header");
-var sticky = header.offsetTop;
+var headerWrap = document.querySelector(".main-header__wrap");
+var sticky = headerWrap.offsetTop;
 
 window.onscroll = function() {stickHead()};
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickHead() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("main-header--stick");
-    header.nextElementSibling.offsetTop = header.offsetHeight;
+    headerWrap.classList.add("main-header__wrap--stick");
+    headerWrap.parentElement.nextElementSibling.offsetTop = headerWrap.offsetHeight;
   } else {
-    header.classList.remove("main-header--stick");
-    header.nextElementSibling.offsetTop = 0;
+    headerWrap.classList.remove("main-header__wrap--stick");
+    headerWrap.parentElement.nextElementSibling.offsetTop = 0;
   }
 }
